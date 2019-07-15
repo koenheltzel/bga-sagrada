2b2f58775855bbbdc2723af0a58eb6d9f2d9d66a
k.heltzel <k.heltzel@moonenpackaging.com>
2019-07-15 14:13:31 +0200

    Made StateSelectPatternTrait non-dynamic temporarily to prevent bugs with getCurrentPlayerId or whatever. dbmodel.sql: fixedd commented out query (# is not valid)

--- modules/States/StateSelectPatternTrait.php
+++ modules/States/StateSelectPatternTrait.php
@@ -14,11 +14,19 @@ trait StateSelectPatternTrait {
             WHERE player_id = {$current_player_id}";
         $playerSagData = self::db($sql)->fetch_assoc();
         $result = [];
-        $result['patterns'] = Patterns::getPatterns(explode(',', $playerSagData['sag_patterns']));
-        $result['privateobjectives'] = explode(',', $playerSagData['sag_privateobjectives']);
+//        $current_player_id = self::getCurrentPlayerId();
+//        $sql = "
+//            SELECT sag_patterns, sag_privateobjectives
+//            FROM player
+//            WHERE player_id = {$current_player_id}";
+//        $playerSagData = self::db($sql)->fetch_assoc();
+//        $result['patterns'] = Patterns::getPatterns(explode(',', $playerSagData['sag_patterns']));
+//        $result['privateobjectives'] = explode(',', $playerSagData['sag_privateobjectives']);
 
+
+        $result['patterns'] = Patterns::getPatterns([1,2,3,4]);
+        $result['privateobjectives'] = ['R'];
 //        print "<PRE>" . print_r($result, true) . "</PRE>";
-
         return $result;
     }
 }
